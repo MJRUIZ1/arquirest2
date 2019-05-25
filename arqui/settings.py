02e0a5e396f491b8a742429c5f37ea182b41c05f
GiselaArevalo <43218775+GiselaArevalo@users.noreply.github.com>
2019-05-25 13:12:05 -0500

    primer commit

--- arqui/settings.py
+++ arqui/settings.py
@@ -95,16 +95,18 @@ WSGI_APPLICATION = 'arqui.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'arquidb',
         'USER': 'postgres',
-        'PASSWORD': '14072017',
+        'PASSWORD': '290597',
         'HOST': 'localhost',
         'PORT': '5432'
     }
 }
+
 
 
 # Password validation
