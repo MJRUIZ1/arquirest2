9478fd1bdab97b38e1fc5f704815ecf65cfa4529
GiselaArevalo <43218775+GiselaArevalo@users.noreply.github.com>
2019-05-31 13:49:45 -0500

    modificacion del settings dos

--- arqui/settings.py
+++ arqui/settings.py
@@ -146,7 +146,7 @@ if not DEBUG:
     'default': {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'DB_NAME',
-        'USER': 'DB_NAME',
+        'USER': 'DB_USER',
         'PASSWORD': 'DB_PASSWORD',
         'HOST': 'DB_HOST',
         'PORT': 'DB_PORT'
